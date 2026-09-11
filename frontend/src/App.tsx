00465c18c714206a14f93d8cc8a4c8b09cc01e87
Antony <icebox1306@gmail.com>
2026-09-11 22:37:18 +0530

    Set up React TypeScript frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  const projectCount: number = 5
+
+  return (
+    <div>
+      <h1>DevTrack</h1>
+      <p>Total Projects: {projectCount}</p>
+    </div>
+  )
+}
+
+export default App
